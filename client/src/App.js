b6bb9142a2caf81159ff28c15f7018287b1f5430
Bhanu Khetharpal <bhanu2paul@gmail.com>
2023-08-15 15:17:23 +0530

    sample

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import MyButton from './components/sizeRecommender/button.js'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to My MERN App!</h1>
         <p>This is the client-side of the MERN application.</p>
+        <MyButton />
       </header>
     </div>
   );
