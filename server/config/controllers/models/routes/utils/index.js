b6b7df382168a85f97767e7219f83e9d9c43e29a
Bhanu Khetharpal <bhanu2paul@gmail.com>
2023-08-15 12:38:26 +0530

    Setup

---
+++ server/config/controllers/models/routes/utils/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import morgan from "morgan";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(morgan("tiny"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+
