b94847dfb8e66b67646fc86fa260697225fc9f10
Bhanu Khetharpal <bhanu2paul@gmail.com>
2023-08-16 02:42:43 +0530

    added prediction functionality

--- client/src/components/knowYourSize/knowYourSize.jsx
+++ client/src/components/knowYourSize/knowYourSize.jsx
@@ -4,26 +4,31 @@ import "../../styles/knowYourSize.css";
 const KnowYourSize = ({ id }) => {
   const [gender, setGender] = useState(""); // State to store selected gender
   const [bust, setBust] = useState("");
-  const [chest, setChest] = useState("");
   const [waist, setWaist] = useState("");
+  const [hip, setHip] = useState("");
+  const [chest, setChest] = useState("");
   const [neck, setNeck] = useState("");
   const [sleeve, setSleeve] = useState("");
   const [predictedSize, setPredictedSize] = useState("");
-  
+
   const handleGenderChange = (event) => {
     setGender(event.target.value);
   };
 
   const handleSubmit = async () => {
     try {
-      // Prepare data to send to the backend
-      const data = {
-        gender,
-        // ... other measurement data
-      };
+      let apiname = "";
+      let data = "";
+      if (gender == female) {
+        apiname = "women-size-chart";
+        data = { bust, waist, hip };
+      } else {
+        apiname = "men-size-chart";
+        data = { chest, waist, neck, sleeve };
+      }
 
       // Make an API request to the backend
-      const response = await fetch("/api/predictSize", {
+      const response = await fetch("/api/" + apiname + "/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -40,9 +45,8 @@ const KnowYourSize = ({ id }) => {
     }
   };
 
-
   return (
-    <section id = {id}>
+    <section id={id}>
       <div className="label">
         <div className="know-your-size"> Know Your Size </div>
       </div>
@@ -57,25 +61,57 @@ const KnowYourSize = ({ id }) => {
         {gender === "female" && (
           <div>
             <label>Bust:</label>
-            <input type="text" value={bust} onChange={(e) => setBust(e.target.value)} />
-            <label>Chest:</label>
-            <input type="text" value={chest} onChange={(e) => setChest(e.target.value)} />
+            <input
+              type="text"
+              value={bust}
+              onChange={(e) => setBust(e.target.value)}
+            />
             <label>Waist:</label>
-            <input type="text" value={waist} onChange={(e) => setWaist(e.target.value)} />
+            <input
+              type="text"
+              value={waist}
+              onChange={(e) => setWaist(e.target.value)}
+            />
+            <label>Hip:</label>
+            <input
+              type="text"
+              value={hip}
+              onChange={(e) => setHip(e.target.value)}
+            />
           </div>
         )}
 
         {gender === "male" && (
           <div>
+            <label>Chest:</label>
+            <input
+              type="text"
+              value={chest}
+              onChange={(e) => setChest(e.target.value)}
+            />
+            <label>Waist:</label>
+            <input
+              type="text"
+              value={waist}
+              onChange={(e) => setWaist(e.target.value)}
+            />
             <label>Neck:</label>
-            <input type="text" value={neck} onChange={(e) => setNeck(e.target.value)} />
+            <input
+              type="text"
+              value={neck}
+              onChange={(e) => setNeck(e.target.value)}
+            />
             <label>Sleeve:</label>
-            <input type="text" value={sleeve} onChange={(e) => setSleeve(e.target.value)} />
+            <input
+              type="text"
+              value={sleeve}
+              onChange={(e) => setSleeve(e.target.value)}
+            />
           </div>
         )}
       </div>
-      {/* <button onClick={handleSubmit}>Submit</button> */}
-      {/* {predictedSize && <p>Predicted Size: {predictedSize}</p>} */}
+      <button onClick={handleSubmit}>Submit</button>
+      {predictedSize && <p>Predicted Size: {predictedSize}</p>}
     </section>
   );
 };
